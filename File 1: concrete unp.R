e99152fca702448630a47d2393ab796338c52b84
Neeraj Pola <neerajpola4@gmail.com>
2024-11-13 12:34:45 +0530

    Create File 1: concrete unp.R

---
+++ File 1: concrete unp.R
@@ -0,0 +1,15 @@
+
+This script includes more detailed exploratory data analysis (EDA) and model experimentation:
+1)Data Loading & Preprocessing:
+Loads the dataset, renames columns, and visualizes data.
+
+2)Exploratory Data Analysis (EDA):
+Correlation matrices and plots using corrplot to analyze variable relationships.
+Scatter plots, hexbin plots, histograms, and boxplots for a detailed visual analysis of various variables.
+
+3)Modeling:
+Builds two linear regression models with different variable selections, calculates MSE, and compares models to select the one with better performance.
+
+4)Prediction:
+Uses the chosen model to predict concrete strength for new data.
+Plots to compare actual vs. predicted concrete strength values in the test dataset.
